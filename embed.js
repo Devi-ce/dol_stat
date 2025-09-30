41788d504a4b7a6cd6221326d64d1cc308e9a28f
Device. <kim_chul_soo@naver.com>
2025-10-01 03:36:53 +0900

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,25 +1,27 @@
-// embed.js
+// embed.js - 티스토리와 동일한 렌더링
 (function() {
-    var id = 'ame-widget-' + Math.random().toString(36).substr(2, 9);
+    // 현재 문서의 너비 감지
+    var containerWidth = document.currentScript.parentElement.offsetWidth || '100%';
     
     document.write(`
-        <div id="${id}" style="width:100%;position:relative;">
-            <iframe 
-                src="https://devi-ce.github.io/dol_stat/"
-                style="width:100%;border:none;display:block;"
-                scrolling="no">
-            </iframe>
+        <style>
+            .ame-widget-container {
+                position: relative;
+                width: 100%;
+                padding-bottom: 130%; /* 위젯 비율 유지 */
+                overflow: hidden;
+            }
+            .ame-widget-container iframe {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border: none;
+            }
+        </style>
+        <div class="ame-widget-container">
+            <iframe src="https://devi-ce.github.io/dol_stat/" scrolling="no"></iframe>
         </div>
     `);
-    
-    // 높이 자동 조절
-    window.addEventListener('message', function(e) {
-        if (e.data && e.data.type === 'ame-widget-height') {
-            var container = document.getElementById(id);
-            if (container) {
-                var iframe = container.querySelector('iframe');
-                iframe.style.height = e.data.height + 'px';
-            }
-        }
-    });
 })();
