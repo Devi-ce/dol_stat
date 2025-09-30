529eb4cb4f0c9a2b1b37f0914f787afb82e9f01b
Device. <kim_chul_soo@naver.com>
2025-10-01 03:35:12 +0900

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,1 +1,25 @@
-document.write('<iframe src="https://devi-ce.github.io/dol_stat/" style="width:100%;height:100vh;border:none;display:block;"></iframe>');
+// embed.js
+(function() {
+    var id = 'ame-widget-' + Math.random().toString(36).substr(2, 9);
+    
+    document.write(`
+        <div id="${id}" style="width:100%;position:relative;">
+            <iframe 
+                src="https://devi-ce.github.io/dol_stat/"
+                style="width:100%;border:none;display:block;"
+                scrolling="no">
+            </iframe>
+        </div>
+    `);
+    
+    // 높이 자동 조절
+    window.addEventListener('message', function(e) {
+        if (e.data && e.data.type === 'ame-widget-height') {
+            var container = document.getElementById(id);
+            if (container) {
+                var iframe = container.querySelector('iframe');
+                iframe.style.height = e.data.height + 'px';
+            }
+        }
+    });
+})();
